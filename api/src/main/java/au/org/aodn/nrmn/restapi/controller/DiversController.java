96b77e53d103694e358d89d85e8378fe9e567a9b
Alex McKeown <a.mckeown@utas.edu.au>
2020-12-16 11:05:52 +1100

    sites start

--- api/src/main/java/au/org/aodn/nrmn/restapi/controller/DiversController.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/controller/DiversController.java
@@ -1,8 +1,6 @@
 package au.org.aodn.nrmn.restapi.controller;
 
 import au.org.aodn.nrmn.restapi.service.CsvService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/divers")
@@ -23,5 +22,16 @@ public class DiversController {
     public void getCsv(final HttpServletResponse response) throws IOException {
         response.setContentType("application/csv");
         csvService.getDiversCsv(response.getWriter());
+    }
+
+
+    @GetMapping("/sites.csv")
+    public void getSitesCsv(final HttpServletResponse response, List<String> siteCodes) throws IOException {
+        response.setContentType("application/csv");
+        csvService.getSitesCsv(response.getWriter(),
+                null,
+                null,
+                null,
+                siteCodes);
     }
 }
--- api/src/main/java/au/org/aodn/nrmn/restapi/service/CsvService.java
+++ api/src/main/java/au/org/aodn/nrmn/restapi/service/CsvService.java
@@ -1,27 +1,36 @@
 package au.org.aodn.nrmn.restapi.service;
 
 import au.org.aodn.nrmn.restapi.model.db.Diver;
+import au.org.aodn.nrmn.restapi.model.db.Location;
+import au.org.aodn.nrmn.restapi.model.db.Site;
 import au.org.aodn.nrmn.restapi.repository.DiverRepository;
+import au.org.aodn.nrmn.restapi.repository.SiteRepository;
+import cyclops.companion.Streams;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
 @Service
 public class CsvService {
     private static final CSVFormat DIVERS_FORMAT = CSVFormat.DEFAULT.withHeader("INITIALS", "FULL NAME");
+    private static final CSVFormat SITES_FORMAT = CSVFormat.DEFAULT.withHeader("SITE", "Site Name", "Latitude", "Longitude", "Region");
 
     @Autowired
     DiverRepository diverRepository;
+    @Autowired
+    SiteRepository siteRepository;
 
     public void getDiversCsv(PrintWriter writer) throws IOException {
         CSVPrinter csvPrinter = DIVERS_FORMAT.print(writer);
@@ -34,5 +43,40 @@ public class CsvService {
 
     private List<String> getDiverAsCsvRecord(Diver diver) {
         return Arrays.asList(diver.getInitials(), diver.getFullName());
+    }
+
+    public void getSitesCsv(PrintWriter writer,
+                            Collection<Location> locations,
+                            Collection<String> provinces,
+                            Collection<String> states,
+                            Collection<String> siteCodes) throws IOException {
+        CSVPrinter csvPrinter = SITES_FORMAT.print(writer);
+
+
+        Stream<String> siteCodesFromProvinces = provinces == null ? Stream.empty() : provinces.stream()
+                .distinct()
+                .flatMap(p -> siteRepository.findSiteCodesByProvince(p).stream());
+
+        Stream<Site> sites = (siteCodes == null
+                ? siteCodesFromProvinces
+                : Streams.concat(siteCodesFromProvinces, siteCodes.stream()))
+                .distinct()
+                .flatMap(sc -> siteRepository.findAll(Example.of(Site.builder().siteCode(sc).build())).stream());
+
+        sites.distinct().sorted(Comparator.comparing(Site::getSiteCode));
+
+    }
+
+    private List<String> getSiteAsCsvRecord(Site site) {
+        return Arrays.asList(
+                site.getSiteCode(),
+                site.getSiteName(),
+                toString(site.getLatitude()),
+                toString(site.getLongitude()),
+                site.getLocation().getLocationName());
+    }
+
+    private String toString(Object couldBeNull) {
+        return couldBeNull == null ? null : couldBeNull.toString();
     }
 }
